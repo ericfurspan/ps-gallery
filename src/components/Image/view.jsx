412a1db3516417816fa65222d609c6369c5d77c7
Eric Furspan <eric.furspan@gmail.com>
2019-05-26 20:45:42 -0400

    Create Image component

---
+++ src/components/Image/view.jsx
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import spinner from '../../spinner.svg';
+
+class Image extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { intersected: false };
+    this.observer = null;
+  }
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(entries => {
+      const image = entries[0];
+      if (image.isIntersecting) {
+        this.setState({ intersected: true });
+        this.observer.disconnect();
+      }
+    });
+    this.observer.observe(this.imgTag);
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+  }
+
+  render() {
+    const { image } = this.props;
+
+    return (
+      <div className="image-wrapper">
+        <div className="intro-text">
+          <p>{image.TAG}</p>
+          <p>{image.DATE}</p>
+        </div>
+        <img
+          src={this.state.intersected ? image.IMAGE : spinner}
+          alt={image.DESCRIPTION || ''}
+          ref={elem => (this.imgTag = elem)}
+          style={this.state.intersected ? {width: '100%'} : {width: '50%'}}
+        />
+        <p>{image.DESCRIPTION}</p>
+      </div>
+    );
+  }
+}
+
+export default Image;
